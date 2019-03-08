6cf7e58ab7060d427c35eb30bc493f772bcc3019
Xuanwo <xuanwo.cn@gmail.com>
2019-03-08 11:29:18 +0800

    paper: template: Use hyper link instead

--- paper/template.go
+++ paper/template.go
@@ -2,9 +2,9 @@ package paper
 
 const readme = `## Papers
 
-| Title | Source | Terms | URL |
-|:--------|:--------|:--------|:--------|
+| Title | Source | Terms |
+|:--------|:--------|:--------|
 {{- range $_, $v := .Papers}}
-|{{$v.Title}}|{{$v.Source}}|{{$v.Terms}}|{{$v.URL}}|
+|[{{$v.Title}}]({{$v.URL}})|{{$v.Source}}|{{$v.Terms}}|
 {{- end}}
 `
